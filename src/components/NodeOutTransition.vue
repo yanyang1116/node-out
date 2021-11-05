4f8873b54679baa291e3630d453dc9de2a41ac64
颜阳 <yanyang@longfor.com>
2021-11-05 18:51:07 +0800

    feat: dev

---
+++ src/components/NodeOutTransition.vue
@@ -0,0 +1,18 @@
+<template>
+  <div><slot /></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class NodeOutTransition extends Vue {
+  @Prop() private source!: [];
+  private beforeUpdate(value: any, oldValue: any) {
+    console.log(value, oldValue, 7689087);
+  }
+  // @Prop({ type: String, default: "" }) private msg!: string;
+}
+</script>
+
+<style scoped></style>
